0651b17469fe1390100341d0dc016dfb19d6d8ae
rkoven74 <roxannekoven@gmail.com>
2019-02-01 14:43:27 -0700

    font changes

--- public/app.js
+++ public/app.js
@@ -3,7 +3,8 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>")
+    $("p").css({"border-style": "solid", "border-width": "3px", "font-size": "25px", "color": "blue", "border-color": "black" });
   }
 });
 
