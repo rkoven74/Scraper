b62b7864bc04a8dfe524dae547bc38dee1b84494
rkoven74 <roxannekoven@gmail.com>
2019-02-01 14:14:04 -0700

    updates

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3008;
+var PORT = process.env.PORT || 3008;
 
 // Initialize Express
 var app = express();
